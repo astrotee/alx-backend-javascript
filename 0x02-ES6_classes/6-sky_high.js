770a4621f196b38af1381c10c23352f0cb63d0a1
amin ahmed <aminahmed24@gmail.com>
2024-10-05 03:17:00 +0300

    6. Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  set floors(n) {
+    if (typeof n !== 'number') {
+      throw new Error('floors must be a number');
+    }
+    this._floors = n;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
