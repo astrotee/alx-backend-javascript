efe18d1076a979836d014e5dfb25b87cc5bb9b9c
amin ahmed <aminahmed24@gmail.com>
2024-10-08 21:02:31 +0300

    count current query

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,9 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const load = weakMap.get(endpoint) || 0;
-  if (load && load >= 5) {
+  const load = (weakMap.get(endpoint) || 0) + 1;
+  weakMap.set(endpoint, load);
+  if (load >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, load + 1);
 }
