f74cdd91ee864bc33c793e1da1804360a448fe30
amin ahmed <aminahmed24@gmail.com>
2024-10-11 21:20:16 +0300

    4. Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,4 +17,32 @@ interface printTeacherFunction {
 
 let printTeacher: printTeacherFunction = function (firstName: string, lastName: string) : string {
   return `${firstName[0]}. ${lastName}`
+};
+
+interface IStudentClass {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudentClass {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+interface IStudentClassConstructor {
+  new(firstName: string, lastName: string) : StudentClass;
 }
